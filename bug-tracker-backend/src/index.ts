18150e37d0a17554527346b6e0845062dd0c6888
andreiserban123 <andyspeed2003@gmail.com>
2024-10-19 12:39:54 +0300

    make it work with import

--- bug-tracker-backend/src/index.ts
+++ bug-tracker-backend/src/index.ts
@@ -1,26 +1,28 @@
-import { Request, Response } from "express";
+import express, { Express, Request, Response } from "express";
+import dotenv from "dotenv";
+import morgan from "morgan";
+import cors from "cors";
 
-const express = require("express");
-const dotenv = require("dotenv");
-const morgan = require("morgan");
-const cors = require("cors");
-
+// Load environment variables
 dotenv.config();
 
-const app = express();
-const port = process.env.PORT || 8080;
+const app: Express = express();
+const port: number = parseInt(process.env.PORT || "8080", 10);
 
+// Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan("dev"));
 app.use(cors());
 
+// Routes
 app.get("/", (req: Request, res: Response) => {
   res.json({ message: "Hello World!" });
 });
 
+// Start server
 app.listen(port, () => {
   console.log(`[server]: Server is running at http://localhost:${port}`);
 });
 
-module.exports = app;
+export default app;
