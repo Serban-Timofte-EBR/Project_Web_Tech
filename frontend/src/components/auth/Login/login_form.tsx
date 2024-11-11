15b0c895c9887ffbef39dffe28fb13063bc48d49
serbantimofte <serban-timofte@outlook.com>
2024-11-11 17:59:13 +0200

    Fix design bug on login

--- frontend/src/components/auth/Login/login_form.tsx
+++ frontend/src/components/auth/Login/login_form.tsx
@@ -1,6 +1,5 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import "./login_form.css";
-import axios from "axios";
 
 interface LoginFormProps {
   onSubmit: (email: string, password: string) => void;
@@ -9,75 +8,33 @@ interface LoginFormProps {
 const LoginForm: React.FC<LoginFormProps> = ({ onSubmit }) => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [backgroundImg, setBackgroundImg] = useState("");
-
-  const accessKey = process.env.UNSPLASH_ACCESS_KEY;
 
   const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault();
     onSubmit(email, password);
   };
 
-  // const fetchRandomBackgroundImg = async () => {
-  //   try {
-  //     const response = await axios.get(
-  //       "https://source.unsplash.com/photos/random",
-  //       {
-  //         headers: {
-  //           Authorization: `Client-ID ${accessKey}`,
-  //         },
-  //         params: {
-  //           query: "technology",
-  //           orientation: "landscape",
-  //         },
-  //       }
-  //     );
-  //     setBackgroundImg(response.data.urls.full);
-  //   } catch (err) {
-  //     console.error("Error fetching backgroung imagine: ", err);
-  //   }
-  // };
-
-  // useEffect(() => {
-  //   fetchRandomBackgroundImg();
-
-  //   const interval = setInterval(() => {
-  //     fetchRandomBackgroundImg();
-  //   }, 10000);
-
-  //   return () => clearInterval(interval);
-  // }, []);
-
   return (
-    <div
-      className="form-container"
-      style={{
-        backgroundImage: `url(${backgroundImg})`,
-        backgroundSize: "cover",
-        backgroundPosition: "center",
-      }}
-    >
-      <div className="form-container">
-        <h1>Login Page</h1>
-        <form onSubmit={handleSubmit}>
-          <label>Email:</label>
-          <input
-            type="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            placeholder="Enter your email"
-          />
-          <label>Password:</label>
-          <input
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            placeholder="Enter your password"
-          />
-          <button type="submit">Login</button>
-        </form>
-        <a href="/create-user-account">Create an account</a>
-      </div>
+    <div className="form-container">
+      <h1>Login Page</h1>
+      <form onSubmit={handleSubmit}>
+        <label>Email:</label>
+        <input
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          placeholder="Enter your email"
+        />
+        <label>Password:</label>
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          placeholder="Enter your password"
+        />
+        <button type="submit">Login</button>
+      </form>
+      <a href="/create-user-account">Create an account</a>
     </div>
   );
 };
